728c619c2855f888b0822a93417455d0bf92ca03
wendlinger <karlw.maia@gmail.com>
2023-12-09 11:18:45 -0300

    Creating project

---
+++ src/main/java/com/cleanArchitecture/CRUDCleanArchitecture/CrudCleanArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.cleanArchitecture.CRUDCleanArchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudCleanArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudCleanArchitectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cleanArchitecture/CRUDCleanArchitecture/CrudCleanArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleanArchitecture.CRUDCleanArchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudCleanArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
